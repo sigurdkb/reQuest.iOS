84299cb34cf14b406ac0af4df1ea77b2f07473c0
Sigurd Brinch <sigurd.k.brinch@uia.no>
2017-02-06 10:18:51 +0100

    Initial Commit

---
+++ Constants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace reQuest.iOS
+{
+	public class Constants
+	{
+		// Azure app-specific connection string and hub path
+		public const string ConnectionString = "Endpoint=sb://request.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=xraI9FX+xfnRqtBXklR3za8cfNzsEHPt00ysDDpxcGw=";
+		public const string NotificationHubPath = "request";
+	}
+}
---
+++ ViewController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace reQuest.iOS
+{
+	public partial class ViewController : UIViewController
+	{
+		//UIWebView webView;
+
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+			Title = "reQuest";
+			View.BackgroundColor = UIColor.Black;
+
+			//webView = new UIWebView(View.Bounds);
+			//View.AddSubview(webView);
+
+			var url = "http://agder-ikt85.uia.no"; // NOTE: https required for iOS 9 ATS (overidden in info.plist)
+			webView.LoadRequest(new NSUrlRequest(new NSUrl(url)));
+
+			// if this is false, page will be 'zoomed in' to normal size
+			webView.ScalesPageToFit = true;
+
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
